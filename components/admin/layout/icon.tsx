d15234cd9a8c43a9570c7c140604f5ad19e229fb
Alejandro Romero <admin@differentgrowth.com>
2025-11-07 18:21:49 +0100

    fix icon size

--- components/admin/layout/icon.tsx
+++ components/admin/layout/icon.tsx
@@ -1,3 +1,3 @@
 import { HouseLineIcon } from "@phosphor-icons/react/dist/ssr";
 
-export const Icon = () => <HouseLineIcon size={18} weight="duotone" />;
+export const Icon = () => <HouseLineIcon size={16} weight="duotone" />;
